ea3cb2a1e4c2cc77a5c43efc72c79542113de3b8
volynkinn <r.volynkin@gmail.com>
2019-10-08 17:39:06 +0300

    Add new code in HelloWorld, and add Newclass

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!!!!1_");
-		System.out.println("Hello All");
+        System.out.println("Hello_World!!!!!!1_");
+		System.out.println("Hello_All");
     }
 }
