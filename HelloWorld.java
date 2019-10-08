cde8aa5393c10990458351645fb64b4689d42488
volynkinn <r.volynkin@gmail.com>
2019-10-08 17:58:54 +0300

    Add new message 'Hello'

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello_World!!!!!!1_");
 		System.out.println("Hello_All");
+		System.out.println("Hello");
     }
 }
